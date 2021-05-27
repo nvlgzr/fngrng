37291fa22fd2c8c9def0cbc124f1e5fe5cf7e978
Elbong Gearny <elbong@navelgazer.club>
2021-05-27 15:57:05 -0700

    Extracted MappingToggle

--- src/App.svelte
+++ src/App.svelte
@@ -3,17 +3,11 @@
   import LevelSelect from "./LevelSelect.svelte";
   import CustomInput from "./CustomInput.svelte";
   import Main from "./Main.svelte";
+  import MappingToggle from "./MappingToggle.svelte";
 </script>
 
 <TopNav />
 <LevelSelect />
 <CustomInput />
 <Main />
-
-<div id="mappingToggle">
-  <h6>Keyboard Mapping: <span>off</span></h6>
-  <label class="switch">
-    <input type="checkbox" autocomplete="off" />
-    <span class="slider" />
-  </label>
-</div>
+<MappingToggle />
