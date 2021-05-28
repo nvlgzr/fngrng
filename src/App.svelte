05308d8177cb547609b51fc3f838a78bcb248e8e
Elbong Gearny <elbong@navelgazer.club>
2021-05-27 19:46:14 -0700

    💔 Added in js files, all strung together with some errors which (I'm guessing) are due to the original author not using strict mode ¯\_(ツ)_/¯

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,16 @@
 <script>
+  import { onMount } from "svelte";
+
   import TopNav from "./TopNav.svelte";
   import LevelSelect from "./LevelSelect.svelte";
   import CustomInput from "./CustomInput.svelte";
   import Main from "./Main.svelte";
   import MappingToggle from "./MappingToggle.svelte";
+  import j from "./app.js";
+
+  onMount(async () => {
+    j();
+  });
 </script>
 
 <TopNav />
