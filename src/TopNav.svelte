97cdd4554c35eb9c53b24ceba1dfc57e3c8a2969
Elbong Gearny <elbong@navelgazer.club>
2021-06-27 18:45:35 -0700

    Added janky-ass* keyboard shortcuts to dropdown
    *Don't mind me. I'm just stroppy about the apparent lack of interest in creating drop-in <select> replacements allowing truly custom components for each <option>. (Or maybe I just used the wrong search terms?) Anyway, having researched for an hour, I'm giving up. It's a fun problem to tackle perhaps, but I'm not ready for a new project of that scale at the moment!

--- src/TopNav.svelte
+++ src/TopNav.svelte
@@ -4,17 +4,17 @@
 </script>
 
 <select bind:value={$currentLayout} autocomplete="off">
-  <option value="colemak">Colemak</option>
-  <option value="colemakdh">Colemak-DH</option>
+  <option value="colemak">⇧⌃1 — Colemak</option>
+  <option value="colemakdh">⇧⌃2 — Colemak-DH</option>
   <option disabled />
-  <option value="azerty">AZERTY</option>
-  <option value="dvorak">Dvorak</option>
-  <option value="lefthandeddvorak">Left-handed Dvorak</option>
-  <option value="qwerty">QWERTY</option>
-  <option value="tarmak">Tarmak</option>
-  <option value="workman">Workman</option>
+  <option value="azerty">⇧⌃A — AZERTY</option>
+  <option value="dvorak">⇧⌃D — Dvorak</option>
+  <option value="lefthandeddvorak">⇧⌃L — Left-handed Dvorak</option>
+  <option value="qwerty">⇧⌃Q — QWERTY</option>
+  <option value="tarmak">⇧⌃T — Tarmak</option>
+  <option value="workman">⇧⌃W — Workman</option>
   <option disabled />
-  <option value="custom">Custom</option>
+  <option value="custom">⇧⌃C &nbsp; Custom</option>
 </select>
 
 <PreferenceMenu />
