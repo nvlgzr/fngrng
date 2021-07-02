b0a867a5c89fc24538be6f0109fef8de9939502d
Elbong Gearny <elbong@navelgazer.club>
2021-07-02 01:44:49 -0700

    minor: logo tweaks

--- src/Nav.svelte
+++ src/Nav.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <nav class:wireframe>
-  <div class="tracking-widest text-gray-700">fngrng</div>
+  <div class="tracking-wide text-gray-700 logo">fngrng</div>
   <div class="col-span-6 flex justify-between">
     <div>level 1 <span class="text-white">arstneio</span></div>
     <div>0/60 0m 0s</div>
@@ -18,6 +18,10 @@
     @apply text-4xl text-gray-400;
     @apply py-7 px-10;
     @apply grid grid-cols-8 gap-4;
+  }
+  .logo {
+    /* I dunno why, but it feels anemic without a slight size boost */
+    font-size: 2.35rem;
   }
   .wireframe {
     outline: solid 1px blueviolet;
