44def76e62885ba1cd21384e21a5495d851e86b9
Elbong Gearny <elbong@navelgazer.club>
2021-07-02 01:26:15 -0700

    Stubbed Prompt

--- src/Prompt.svelte
+++ src/Prompt.svelte
@@ -1,11 +1,28 @@
 <script>
+  import { showFrames } from "./persistentStore";
+
+  $: wireframe = $showFrames;
 </script>
 
-<div>Prompt</div>
+<section class:wireframe>
+  <div>stated stood routes recorded heat crash</div>
+  <div>hat horses assured statutory trash trees</div>
+  <div>hours started addressed tours houses</div>
+</section>
 
-<style>
+<style lang="postcss">
+  section {
+    @apply text-center;
+    @apply font-serif;
+    @apply text-5xl;
+    @apply p-6;
+    @apply grid grid-cols-8 gap-4;
+  }
   div {
-    background-color: slateblue;
-    opacity: 0.5;
+    @apply col-start-3 col-span-6;
+    @apply text-left;
+  }
+  .wireframe {
+    outline: solid 1px goldenrod;
   }
 </style>
