493c84d85b6753811fee38bc44618b7fb7c9733d
Elbong Gearny <elbong@navelgazer.club>
2021-07-06 01:59:59 -0700

    Blank levels after Level 1 get labelled
    Hopefully this will be enough to prompt more level setting. And if not, no biggie. Long-term, I want to make the whole leveling thing go away, anyway.

--- src/CurrentLevel.svelte
+++ src/CurrentLevel.svelte
@@ -1,11 +1,32 @@
 <script>
-  import { currentLevel } from "./persistentStore";
+  import { alphabet } from "./levelMappings";
+
+  import {
+    currentLayout,
+    currentLevel,
+    letterSetsForCurrentLayout,
+    levelMaps,
+  } from "./persistentStore";
+  import { leftMinusRight } from "./pureFunctions";
   import { lettersInLevel } from "./volatileStore";
+
+  $: levelLetters = leftMinusRight(alphabet, $lettersInLevel).length
+    ? $letterSetsForCurrentLayout[$currentLevel - 1]
+    : "all letters";
 </script>
 
 <div>
-  level {$currentLevel} <span class="text-white">{$lettersInLevel}</span>
-  {#if $lettersInLevel === ""}
-    <span class="text-red-400 text-2xl">⇐ select a level…</span>
+  level {$currentLevel}
+  <span class="text-white">{levelLetters}</span>
+  <!-- There are corner cases where the toggles below -->
+  <!-- don't quite make perfect sense, but under "normal"-->
+  <!-- conditions I think this should work perfectly -->
+  <!-- (at least for now). -->
+  {#if $currentLayout === "custom" && levelLetters === ""}
+    {#if $currentLevel === 1}
+      <span class="text-red-400 text-2xl">⇐ select a level to edit…</span>
+    {:else}
+      <span class="text-red-400 text-2xl">empty level</span>
+    {/if}
   {/if}
 </div>
