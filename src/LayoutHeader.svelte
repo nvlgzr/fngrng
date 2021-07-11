0a7816ea06f6672103700a313de0614de8c1d345
Elbong Gearny <elbong@navelgazer.club>
2021-07-11 01:46:17 -0700

    IN PROGRESS - Layout menu

--- src/LayoutHeader.svelte
+++ src/LayoutHeader.svelte
@@ -1,18 +1,74 @@
 <script>
+  import Hoverable from "./Hoverable.svelte";
+  import MenuItem from "./MenuItem.svelte";
   import { currentLayout, showFrames } from "./persistentStore";
 
   $: wireframe = $showFrames;
+  // class:wireframe
+
+  let showMenu = false;
+  const handleClick = (e) => {
+    showMenu = !showMenu;
+  };
+
+  const layouts = [
+    { title: "colemak", shortcut: "1" },
+    { title: "colemakdh", shortcut: "2" },
+  ];
 </script>
 
-<div class:wireframe>{$currentLayout}</div>
+<Hoverable let:hovering>
+  <section class:hovering on:click={handleClick}>
+    <div class="anchor" class:show-menu={showMenu}>
+      {$currentLayout}
+      <span class:hovering>﹀</span>
+    </div>
+    {#if showMenu}
+      <div class="menu">
+        {#each layouts as layout}
+          <MenuItem
+            shortcut={layout.shortcut}
+            callback={() => {
+              console.log(layout.title);
+            }}>{layout.title}</MenuItem
+          >
+        {/each}
+      </div>
+    {/if}
+  </section>
+</Hoverable>
 
-<style>
-  div {
+<style lang="postcss">
+  section {
+    @apply relative flex justify-center;
     @apply text-center;
     @apply text-6xl;
     @apply p-6;
     @apply text-gray-400;
+    @apply cursor-pointer;
   }
+
+  .hovering {
+    @apply text-green-400 opacity-100;
+  }
+
+  .show-menu {
+    @apply filter brightness-90;
+  }
+
+  span {
+    @apply text-4xl font-black;
+    @apply opacity-0 align-bottom;
+  }
+
+  .menu {
+    @apply absolute z-50;
+    @apply p-6 rounded-b-sm shadow-lg;
+    @apply bg-gray-50;
+    @apply text-3xl;
+    top: 6rem;
+  }
+
   .wireframe {
     outline: solid 1px orange;
   }
---
+++ src/MenuItem.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let shortcut = "";
+  export let callback = () => {};
+</script>
+
+<div class="menu-item" on:click={callback}>
+  <slot />
+  <div class="shortcut">&nbsp;&nbsp;&nbsp;⇧⌃{shortcut}</div>
+</div>
+
+<style lang="postcss">
+  .menu-item {
+    @apply flex justify-between;
+    @apply p-3;
+  }
+
+  .menu-item:hover {
+    @apply filter brightness-90;
+  }
+</style>
