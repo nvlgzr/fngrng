1cac96a78bdd08f0d812b756d71944a123d20ec5
Elbong Gearny <elbong@navelgazer.club>
2021-07-24 20:20:00 -0700

    Added fake cursor to Input
    Noticed some confusion during user testing about the difference between Input & Display. Hopefully this blinking line is enough to reduce such confusion.

--- src/Input.svelte
+++ src/Input.svelte
@@ -1,4 +1,6 @@
 <script>
+  import { gameState } from "./volatileStore";
+
   export let text = "";
   export let redden = false;
 </script>
@@ -14,6 +16,9 @@
   {:else}
     &nbsp;
   {/if}
+  {#if $gameState === "on"}
+    <span> | </span>
+  {/if}
 </div>
 
 <style lang="postcss">
@@ -21,7 +26,24 @@
     @apply text-center text-6xl;
     @apply p-16 mb-4;
   }
+  span {
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+      Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+    animation: blink 0.9s infinite;
+    @apply font-extralight relative -left-4;
+  }
   .redden {
     @apply text-red-400;
+  }
+  @keyframes blink {
+    0% {
+      opacity: 0.6;
+    }
+    60% {
+      opacity: 0.6;
+    }
+    100% {
+      opacity: 0;
+    }
   }
 </style>
