3864b19b6062557dc31f0f0955897b22e223c3ac
garch <garch@navelgazer.club>
2021-08-12 16:55:03 -0700

    Fixed line-height* bug in Scroller introduced by font change
    *Tailwind calls it leading.

--- src/Scroller.svelte
+++ src/Scroller.svelte
@@ -67,7 +67,7 @@
     -webkit-mask-image: linear-gradient(to right, black 75%, transparent);
   }
   div {
-    @apply whitespace-nowrap relative overflow-hidden h-14;
+    @apply whitespace-nowrap relative overflow-hidden h-14 leading-none;
   }
   .scroll {
     /*
